009bee2edbcb0a30347d209dbbc2e272f8106017
jzbor <jzbor@posteo.net>
2025-06-24 21:27:44 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "jzbor's NixOS port for the pinenote";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: inputs.blueprint {
+    inherit inputs;
+    systems = [ "aarch64-linux" ];
+  };
+}

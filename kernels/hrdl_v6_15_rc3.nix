90ff3995656570be1fff535b16b76fe7eb275546
jzbor <jzbor@posteo.net>
2025-06-25 12:24:24 +0200

    Updating linux hrdl_v6_15_rc3 hash

--- kernels/hrdl_v6_15_rc3.nix
+++ kernels/hrdl_v6_15_rc3.nix
@@ -5,8 +5,8 @@ pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_15.override {
     src = fetchFromSourcehut {
       owner = "~hrdl";
       repo = "linux";
-      rev = "v6.15-rc3";
-      sha256 = "sha256-wcReSz4PryusANbjMQylnzHXMgmKZ+s4+L57gpEayF0=";
+      rev = "f75fe16d81ae784b8cd2b915113f3a99ff812777";  # v6.15-rc3
+      sha256 = "sha256-DhMiZMcwownJJRqIYOj87E/j34jJZb2/rTOhYuMumG4=";
     };
     version = "6.15.0-rc3";
     modDirVersion = "6.15.0-rc3";

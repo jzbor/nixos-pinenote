3e12862dd76c8edf72b05d8b938d8d06b705c7f7
jzbor <jzbor@posteo.net>
2025-08-03 18:44:50 +0200

    Attempt implementing new waveform systemd service

--- modules/nixos/initial.nix
+++ modules/nixos/initial.nix
@@ -52,38 +52,76 @@ in {
         };
       };
 
-      pinenote-hrdl-convert-waveform = {
+      pinenote-init-convert-waveform = {
         description = "Convert rockchip_ebc ebc.wbf waveform to custom_wf.bin";
-        wantedBy = [ "display-manager.service" ];
-        after = [ "pinenote-hrdl-extract-waveform.service" ];
-        before = [ "display-manager.service" ];
-
+        wantedBy = [ "multi-user.target" ];
         serviceConfig = {
           Type = "oneshot";
-          ExecCondition = concatStringsSep " && " [
-            "${pkgs.coreutils}/bin/test ! -e /lib/firmware/rockchip/custom_wf.bin"
-            "${pkgs.coreutils}/bin/test -e /lib/firmware/rockchip/ebc.wbf"
-          ];
-          ExecStart = "/bin/sh -c '" +(concatStringsSep " && " [
-            "cd /tmp"
-            "${flakePkgs.hrdl-utils}/bin/wbf_to_custom.py ${firmwareDir}/ebc.wbf"
-            "mv custom_wf.bin ${firmwareDir}/custom_wf.bin"
-            # "mkinitcpio -P"  -- Not sure what the NixOS equivalent would be
-            "(modprobe -r rockchip_ebc; modprobe rockchip_ebc)"
-          ]) + "'";
+          ExecStart = "${pkgs.writeShellScript "init-waveform.sh" ''
+            if [ -e ${firmwareDir}/custom_wf.bin ]; then
+              echo "Nothing to do (${firmwareDir}/custom_wf.bin already exists)"
+              exit 0
+            fi
+
+
+            if [ -e ${firmwareDir}/ebc.wbf ]; then
+              echo "Removing old extraction file ${firmwareDir}/ebc.wbf"
+              rm -f "${firmwareDir}/ebc.wbf"
+            fi
+
+
+            echo "Extracting waveforms"
+            mkdir -vp ${firmwareDir}
+            ${flakePkgs.hrdl-utils}/bin/waveform_extract.sh
+
+
+            echo "Converting waveforms"
+            cd /tmp
+            ${flakePkgs.hrdl-utils}/bin/wbf_to_custom.py ${firmwareDir}/ebc.wbf
+            mv -v custom_wf.bin ${firmwareDir}/
+
+
+            echo "Reloading driver"
+            modprobe -v -r rockchip_ebc
+            modprobe -v rockchip_ebc
+
+            echo "Done"
+          ''}";
         };
       };
 
-      pinenote-hrdl-extract-waveform = {
-        description = "Extract rockchip_ebc ebc.wbf waveform from waveform partition";
-        wantedBy = [ "default.target" ];
+      # pinenote-hrdl-convert-waveform = {
+      #   description = "Convert rockchip_ebc ebc.wbf waveform to custom_wf.bin";
+      #   wantedBy = [ "display-manager.service" ];
+      #   after = [ "pinenote-hrdl-extract-waveform.service" ];
+      #   before = [ "display-manager.service" ];
 
-        serviceConfig = {
-          Type = "oneshot";
-          ExecCondition = "${pkgs.coreutils}/bin/test ! -e /lib/firmware/rockchip/ebc.wbf";
-          ExecStart = "${flakePkgs.hrdl-utils}/bin/waveform_extract.sh";
-        };
-      };
+      #   serviceConfig = {
+      #     Type = "oneshot";
+      #     ExecCondition = concatStringsSep " && " [
+      #       "${pkgs.coreutils}/bin/test ! -e /lib/firmware/rockchip/custom_wf.bin"
+      #       "${pkgs.coreutils}/bin/test -e /lib/firmware/rockchip/ebc.wbf"
+      #     ];
+      #     ExecStart = "/bin/sh -c '" +(concatStringsSep " && " [
+      #       "cd /tmp"
+      #       "${flakePkgs.hrdl-utils}/bin/wbf_to_custom.py ${firmwareDir}/ebc.wbf"
+      #       "mv custom_wf.bin ${firmwareDir}/custom_wf.bin"
+      #       # "mkinitcpio -P"  -- Not sure what the NixOS equivalent would be
+      #       "(modprobe -r rockchip_ebc; modprobe rockchip_ebc)"
+      #     ]) + "'";
+      #   };
+      # };
+
+      # pinenote-hrdl-extract-waveform = {
+      #   description = "Extract rockchip_ebc ebc.wbf waveform from waveform partition";
+      #   wantedBy = [ "default.target" ];
+
+      #   serviceConfig = {
+      #     Type = "oneshot";
+      #     ExecCondition = "${pkgs.coreutils}/bin/test ! -e /lib/firmware/rockchip/ebc.wbf";
+      #     ExecStart = "${flakePkgs.hrdl-utils}/bin/waveform_extract.sh";
+      #   };
+      # };
     };
   };
 }

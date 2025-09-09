2a07ef8539e871a8c1e42948e4acfc8f4b363931
jzbor <jzbor@posteo.net>
2025-09-09 23:33:11 +0200

    Adding kernel package for hrdls 6.17.0-rc5 branch

--- misc/kernels/default.nix
+++ misc/kernels/default.nix
@@ -1,7 +1,8 @@
 pkgs:
 
 rec {
-  linuxPackages-pinenote = linuxPackages-hrdl_v6_15_rc3;
+  linuxPackages-pinenote = linuxPackages-hrdl_v6_17_rc5;
   linuxPackages-hrdl_v6_15_rc3 = pkgs.callPackage ./hrdl_v6_15_rc3.nix {};
+  linuxPackages-hrdl_v6_17_rc5 = pkgs.callPackage ./hrdl_v6_17_rc5.nix {};
 }
 

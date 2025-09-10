12553a3aa1c454796748104101e4aadb93b74f1b
jzbor <jzbor@posteo.net>
2025-09-10 14:55:18 +0200

    Update hrdl-utils and add cyttsp5_update_config.py

--- packages/hrdl-utils/default.nix
+++ packages/hrdl-utils/default.nix
@@ -6,6 +6,19 @@ let
     numpy
     dbus-next
     i3ipc
+    crcmod
+    (with pkgs.python3Packages; buildPythonPackage {
+      name = "pylibi2c";
+      version = "unstable";
+      format = "pyproject";
+      propagatedBuildInputs = [ setuptools setuptools-scm ];
+      src = pkgs.fetchFromGitHub {
+        owner = "amaork";
+        repo = "libi2c";
+        rev = "dcdf6c671e84d88ab82a863251e01c024e2fdf94";
+        sha256 = "sha256-ZXP699wDab2wJnHUK6EDojCcXMarvKURP9d6dHNSPck=";
+      };
+    })
   ]);
 in pkgs.stdenvNoCC.mkDerivation {
   name = "hrdl-utils";
@@ -18,8 +31,8 @@ in pkgs.stdenvNoCC.mkDerivation {
   src = pkgs.fetchFromSourcehut {
     owner = "~hrdl";
     repo = "pinenote-dist";
-    rev = "28d2c05ab4e80a7972611cd3188f3e8be4197f6a";
-    sha256 = "sha256-r+Dy4lyu1G4eDdmYAQRJho8J/66Cbp91hGTrsPrIU6A=";
+    rev = "717409008ec30b1673ce0ad615c8ab22f341eda4";
+    sha256 = "sha256-c2aWd++3dAOaTSrm6auL2NhI5LBhI4QYAbXiJpRc2Ys=";
   };
 
   nativeBuildInputs = with pkgs; [ makeWrapper ];
@@ -36,6 +49,7 @@ in pkgs.stdenvNoCC.mkDerivation {
     cp bin/waveform_extract.sh $out/bin/
     cp bin/rockchip_ebc_custom_ioctl.py $out/bin/
     cp bin/sway_dbus_integration.py $out/bin/
+    cp bin/cyttsp5_update_config.py $out/bin/
 
     wrapProgram $out/bin/waveform_extract.sh \
       --prefix PATH : ${lib.makeBinPath (with pkgs; [ coreutils hexdump ])}
